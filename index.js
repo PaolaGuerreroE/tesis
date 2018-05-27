a539a7d8020c821841e37325b2069f91b76ef61c
arturogonza <agonzalez@spsolutions.com.mx>
2018-05-26 20:25:57 -0500

    test dikstra

--- index.js
+++ index.js
@@ -9,14 +9,17 @@ var route = new Graph();
 
 app.get('/', function (req, res) {
 
-    route.addNode('A', { B: 1 });
-    route.addNode('B', { A: 1, C: 2, D: 4 });
-    route.addNode('C', { B: 2, D: 1 });
-    route.addNode('D', { C: 1, B: 4 });
+    route.addNode(node1,{node2:1262});
+    route.addNode(node2,{node3:1158,node118:534});
+    route.addNode(node3,{node4:973});
+    route.addNode(node4,{node5:501});
+    route.addNode(node5,{node6:645});
+    route.addNode(node6,{node7:793});
+    route.addNode(node7,{node8:409});
+    route.addNode(node8,{node9:458,node52:665});
 
-    var response = route.path('A', 'D');
-
-    res.send('Hello World!',response);
+    var response = route.path('node1', 'node8');
+    console.log('esta es la respuesta',response)
 });
 
 
