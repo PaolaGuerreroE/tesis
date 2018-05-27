9df07847b4714b0e9f92c3a31a94f4baf24d5b27
arturogonza <agonzalez@spsolutions.com.mx>
2018-05-26 19:48:05 -0500

    add dijkstra algoritm

--- index.js
+++ index.js
@@ -2,20 +2,36 @@
 'use strict';
 var express = require('express');
 var app = express();
+const route = new Graph()
+
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+    route.addNode('A', { B: 1 });
+    route.addNode('B', { A: 1, C: 2, D: 4 });
+    route.addNode('C', { B: 2, D: 1 });
+    route.addNode('D', { C: 1, B: 4 });
+
+    var response = route.path('A', 'D');
+
+    res.send('Hello World!',response);
 });
 
 
-app.get('/api/:idestacion&:idseveridad', function (req, res) {
+app.get('/api/reporte/:idestacion&:idseveridad', function (req, res) {
     var idestacion = req.params.idestacion;
     var idestacion = req.params.idseveridad;
 
     res.send('Hola Pao');
-  });
+});
 
+app.get('/api/ruta/:idestacion&:idseveridad', function (req, res) {
+    var idestacion = req.params.idestacion;
+    var idestacion = req.params.idseveridad;
+
+    res.send('Hola Pao');
+});
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port !',process.env.PORT);
+    console.log('Example app listening on port !', process.env.PORT);
 });
